4e6d1c8a27a40aa35fc19616aa65a24c9172a19e
Baptiste Arnou <baptiste.arnou@gmail.com>
2024-12-09 20:39:10 +0100

    AOC 09 - 2

--- src/bin/09.rs
+++ src/bin/09.rs
@@ -1,52 +1,8 @@
 advent_of_code::solution!(9);
 
-use std::fmt;
-use std::ops::Mul;
+use std::cmp::{min, max};
 
-#[derive(Clone, Copy)]
-enum Item {
-    Empty,
-    File(i32)
-}
-
-impl fmt::Debug for Item {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        match self {
-            Item::Empty => write!(f, "."),
-            Item::File(value) => write!(f, "{}", value),
-        }
-    }
-}
-
-impl fmt::Display for Item {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        match self {
-            Item::Empty => write!(f, "."),
-            Item::File(value) => write!(f, "{}", value),
-        }
-    }
-}
-
-impl Mul<&Item> for usize {
-    // The multiplication of rational numbers is a closed operation.
-    type Output = Self;
-
-    fn mul(self, rhs: &Item) -> Self {
-        match rhs {
-            Item::Empty => 0,
-            Item::File(value) => self * (*value as usize),
-        }
-    }
-}
-
-impl Item {
-    pub fn is_empty(&self) -> bool {
-        match self {
-            Item::Empty => true,
-            _ => false
-        }
-    }
-}
+type Item = (i32, i32);
 
 fn get_disk_layout(input: &str) -> Vec<Item> {
     let disk = input
@@ -57,46 +13,122 @@ fn get_disk_layout(input: &str) -> Vec<Item> {
             Vec::<Item>::new(),
             |mut d, (i, c)| {
                 if i % 2 == 0 {
-                    let chars = vec![Item::File(i as i32 / 2); c.to_digit(10).unwrap() as usize];
-                    d.extend(chars);
+                    d.push((c.to_digit(10).unwrap() as i32, i as i32 / 2));
                 } else {
-                    let items = vec![Item::Empty; c.to_digit(10).unwrap() as usize];
-                    d.extend(items);
+                    d.push((c.to_digit(10).unwrap() as i32, 0));
                 }
                 d
             }
         );
-    disk
+        disk
 }
 
 fn compute_checksum(disk: &Vec<Item>) -> u128 {
-    let count: u128 = disk
+    let oneline_disk: Vec<i32> = disk
+        .iter()
+        .fold(
+            Vec::<i32>::new(),
+            |mut d, (n, id)| {
+                let v: Vec<i32> = vec![*id; *n as usize].to_vec();
+                d.extend(v);
+                d
+    });
+    let count: u128 = oneline_disk
         .iter()
         .enumerate()
-        .map(|(i, item)| (i * item) as u128)
+        .map(|(i, value)| (i as i32 * value) as u128)
         .sum();
     count
 }
 
-fn reorganise_disk_single_files(disk: &mut Vec<Item>) {
-    let mut first_empty = disk.iter().position(|&item| item.is_empty()).unwrap();
-    let mut last_file  = disk.len() - disk.iter().rev().position(|&item| !item.is_empty()).unwrap() - 1;
-    while first_empty < last_file {
-        disk.swap(first_empty, last_file);
-        first_empty = disk.iter().position(|&item| item.is_empty()).unwrap();
-        last_file  = disk.len() - disk.iter().rev().position(|&item| !item.is_empty()).unwrap() - 1;
+fn reorganise_disk_into_single_files(disk: &Vec<Item>) -> Vec<Item> {
+    let mut cloned_disk = disk.clone();
+    let mut disk_single_files: Vec<Item> = Vec::new();
+    disk_single_files.push(cloned_disk[0]);
+    cloned_disk.remove(0);
+
+    while !cloned_disk.is_empty() {
+        if cloned_disk[0].0 > 0 {
+            let mut n_remaining_spaces = cloned_disk[0].0;
+            let mut to_insert: Vec<Item> = Vec::new();
+            while n_remaining_spaces > 0 {
+                if let Some(found) = cloned_disk.iter().rev().position(|item| item.1 != 0) {
+                    let p: usize = cloned_disk.len() - found - 1;
+                    let last_file = cloned_disk[p];
+                    if last_file.0 > n_remaining_spaces {
+                        cloned_disk[p].0 -= n_remaining_spaces;
+                        to_insert.push((n_remaining_spaces, cloned_disk[p].1));
+                    } else {
+                        to_insert.push(cloned_disk[p]);
+                        cloned_disk.remove(p);
+                    }
+                    n_remaining_spaces -= to_insert.last().unwrap().0;
+                } else {
+                    break;
+                }
+            }
+            disk_single_files.extend(to_insert);
+        }
+        if let Some(item) = cloned_disk.get(1) {
+            if item.1 != 0 {
+                disk_single_files.push(*item);
+            }
+        }
+
+        cloned_disk.drain(0..(min(2, cloned_disk.len())));
     }
+    disk_single_files
+}
+
+fn reorganise_disk_into_block_files(disk: &Vec<Item>) -> Vec<Item> {
+    let mut cloned_disk = disk.clone();
+    let mut disk_block_files: Vec<Item> = Vec::new();
+    disk_block_files.push(cloned_disk[0]);
+    cloned_disk.remove(0);
+
+    while !cloned_disk.is_empty() {
+        if cloned_disk[0].1 == 0 {
+            if cloned_disk[0].0 > 0 {
+                let mut n_remaining_spaces = cloned_disk[0].0;
+                let mut to_insert: Vec<Item> = Vec::new();
+                while n_remaining_spaces > 0 {
+                    if let Some(found) = cloned_disk.iter().rev().position(|item| item.1 != 0 && item.0 <= n_remaining_spaces) {
+                        let p: usize = cloned_disk.len() - found - 1;
+                        to_insert.push(cloned_disk[p]);
+                        cloned_disk[p].1 = 0;
+                        n_remaining_spaces -= to_insert.last().unwrap().0;
+                    } else {
+                        to_insert.push((n_remaining_spaces, 0));
+                        n_remaining_spaces -= n_remaining_spaces;
+                    }
+                }
+                disk_block_files.extend(to_insert);
+            }
+        } else {
+            disk_block_files.push(cloned_disk[0]);
+        }
+        cloned_disk.remove(0);
+        // And remove last items that are empty space
+        if let Some(found) = cloned_disk.iter().rev().position(|item| item.1 != 0) {
+            let p: usize = cloned_disk.len() - found;
+            cloned_disk.drain(p..cloned_disk.len());
+        }
+    }
+    disk_block_files
 }
 
 pub fn part_one(input: &str) -> Option<u128> {
-    let mut disk = get_disk_layout(input);
-    reorganise_disk_single_files(&mut disk);
-    let checksum = compute_checksum(&disk);
+    let disk = get_disk_layout(input);
+    let disk_single = reorganise_disk_into_single_files(&disk);
+    let checksum = compute_checksum(&disk_single);
     Some(checksum)
 }
 
-pub fn part_two(input: &str) -> Option<u32> {
-    None
+pub fn part_two(input: &str) -> Option<u128> {
+    let disk = get_disk_layout(input);
+    let disk_single = reorganise_disk_into_block_files(&disk);
+    let checksum = compute_checksum(&disk_single);
+    Some(checksum)
 }
 
 #[cfg(test)]
